cf87e7052498ef7d342f72c0035352e4b3457ea6
LulzCo <ysw991106@gmail.com>
2023-01-19 10:24:47 +0900

    마커 함수화

--- src/components/common/KaMap/KaMap.jsx
+++ src/components/common/KaMap/KaMap.jsx
@@ -8,25 +8,31 @@ function KaMap() {
     const container = document.getElementById('map');
     const options = {
       center: new kakao.maps.LatLng(33.454705, 126.560767),
-      level: 4,
+      level: 5,
     };
-    // eslint-disable-next-line no-unused-vars
     // 지도 생성 및 출력
+    // eslint-disable-next-line no-unused-vars
     const map = new kakao.maps.Map(container, options);
 
-    const testMarker = setMarker();
+    // const markerPosition = new kakao.maps.LatLng(33.454705, 126.560767);
+    // const marker = new kakao.maps.Marker({
+    //   position: markerPosition,
+    // });
+    // marker.setMap(map);
+    // eslint-disable-next-line no-use-before-define
+    setMarker(map, 33.454705, 126.560767);
   }, []);
 
-  function setMarker() {
-    const { map } = this;
-    // 마커 출력
-    const markerPosition = new kakao.maps.LatLng(33.454705, 126.560767);
+  // function setMarker(map, lat, lng)
+  // parameter => map: 카카오맵 지도 객체, lat: 위도, lng: 경도
+  // 지도 위에 해당 위도와 경도 위치에 맞게 마커 생성
+  function setMarker(map, lat, lng) {
+    const markerPosition = new kakao.maps.LatLng(lat, lng);
     const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
-    return marker.setMap(map);
+    marker.setMap(map);
   }
-
   return (
     <div
       id="map"
