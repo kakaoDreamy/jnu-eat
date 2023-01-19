cff70632a53c56e709d5ebdfdf87c23b2c24e717
hy219 <jhy_0219@daum.net>
2023-01-19 15:39:00 +0900

    app.js에서 router 제거

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,9 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-);
+root.render(<App />);
