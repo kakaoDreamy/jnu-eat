60056fb9a3a0b2a81d4a27232bcc745e22dd9192
Cr28self <tkhoon3study@gmail.com>
2023-01-25 18:47:28 +0900

    머지해결??

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,8 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-=======
-import '../App.css';
->>>>>>> develop
 import GlobalStyle from '../components/common/GlobalStyle';
 import Layout from '../components/common/Layout';
 import Header from '../components/common/Header';
@@ -10,16 +6,9 @@ import StatusBar from '../components/common/StatusBar';
 import MapContents from '../components/Map/MapContents';
 import ProgressBar from '../components/common/ProgressBar/ProgressBar';
 import ResultPage from '../components/ResultPage';
-<<<<<<< HEAD
 import RouletteContents from '../components/Roulette/RouletteContents';
 
 function MainPage() {
-=======
-import KaMap from '../components/common/KaMap/KaMap';
-import RouletteContents from '../components/Roulette/RouletteContents';
-
-function App() {
->>>>>>> develop
   const resname = '백일흑야';
   const [state, setState] = useState({
     curStage: 1,
@@ -27,19 +16,12 @@ function App() {
     time: '',
     rouletteList: '',
     rouletteResult: '',
-<<<<<<< HEAD
     map: '',
-=======
->>>>>>> develop
   });
 
   const nextStage = () => {
     setState({ ...state, curStage: state.curStage + 1 });
   };
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
   console.log(state);
   return (
     <div className="App">
@@ -50,26 +32,16 @@ function App() {
           <ProgressBar curStage={state.curStage} />
         </StatusBar>
         <ResultPage resName={resname} />
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
         {state.curStage === 5 && (
           <div>결과 페이지 : {state.rouletteResult.text}</div>
         )}
 
         {state.curStage !== 5 && state.curStage % 2 === 1 ? (
-<<<<<<< HEAD
           <MapContents
             state={state}
             setState={setState}
             nextStage={nextStage}
           />
-=======
-          <MapContents state={state} setState={setState} nextStage={nextStage}>
-            <KaMap />
-          </MapContents>
->>>>>>> develop
         ) : (
           <RouletteContents
             state={state}
@@ -82,8 +54,4 @@ function App() {
   );
 }
 
-<<<<<<< HEAD
 export default MainPage;
-=======
-export default App;
->>>>>>> develop
